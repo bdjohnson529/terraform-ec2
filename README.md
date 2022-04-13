e1d195dde88679b491aee8c650299c1789c24ea9
Ben <bdjohnson529@gmail.com>
2022-04-12 18:19:00 -0700

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Terraform : EC2
+
+This repo demonstrates the deployment of an EC2 instance via Terraform. Make sure you have both [Terraform](https://www.terraform.io/) and the [AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html) installed.
+
+
+## Setup and Cleanup
+Initialize.
+```bash
+terraform init
+```
+
+Plan infrastructure.
+```bash
+terraform plan
+```
+
+Set up the infrastructure.
+```bash
+terraform apply
+```
+
+Tear down the infrastructure.
+```bash
+terraform destroy
+```
+
+## SSH into EC2
+Modify `key_name` within the `aws_instance` to the name of your key. Set the permissions on your key.
+```bash
+chmod 400 retool-key.pem
+```
+
+Connect to the instance using the public DNS
+```bash
+ssh -i "retool-key.pem" ubuntu@ec2-XX-XX-XXX.us-west-2.compute.amazonaws.com
+```
---
+++ main.tf
@@ -0,0 +1,94 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_default_vpc" "default_vpc" {
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_default_vpc.default_vpc.id
+
+  ingress {
+    description = "ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "http"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "https"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "custom"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "app_server" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.medium"
+  key_name               = "retool-key"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+
+  tags = {
+    app = "retool"
+  }
+}
